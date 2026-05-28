7a7ce339f9a4cade08d42fbcb336b185bbd98c7e
Eugenio Blanco <eblancofe@uoc.edu>
2026-05-28 17:30:39 +0200

    añadir Accented.dev y ajustes finales

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,19 @@ import { appConfig } from './app/app.config';
 import { registerLocaleData } from '@angular/common';
 import { LOCALE_ID } from '@angular/core';
 import localeEs from '@angular/common/locales/es';
+import { environment } from './environments/environment';
 
 registerLocaleData(localeEs);
+
+/* #accented.dev# 
+Nos muestra un chequeo de nuestra página Web que partes entran en conflicto para tener
+en cuenta a la hora de modificar los elementos, para cumplir los requisitos de accesibilidad WCAG
+de momento, se deja comentado, si se descomenta, se comprueba y chequea nuestra aplicación Web "GSXPD"
+if (!environment.production) {
+  const { accented } = await import('accented');
+  accented();
+}
+*/
 
 bootstrapApplication(AppComponent, {
   ...appConfig,
